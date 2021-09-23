fe27789af32f6d4b83940581bc701c9ec523043e
Aliraza <mohamedalirazamsm@gmail.com>
2021-09-24 00:45:22 +0530

    Initial commit

---
+++ assets/layout/pre_loader.php
@@ -0,0 +1,20 @@
+<div class="theme-loader">
+        <div class="ball-scale">
+            <div class='contain'>
+                <div class="ring"><div class="frame"></div></div>
+                <div class="ring"><div class="frame"></div></div>
+                <div class="ring"><div class="frame"></div></div>
+                <div class="ring"><div class="frame"></div></div>
+                <div class="ring"><div class="frame"></div></div>
+                <div class="ring"><div class="frame"></div></div>
+                <div class="ring"><div class="frame"></div></div>
+                <div class="ring"><div class="frame"></div></div>
+                <div class="ring"><div class="frame"></div></div>
+                <div class="ring"><div class="frame"></div></div>
+            </div>
+        </div>
+    </div>
+
+    <div id="pcoded" class="pcoded">
+        <div class="pcoded-overlay-box"></div>
+        <div class="pcoded-container navbar-wrapper">
---
+++ index.php
@@ -0,0 +1,7 @@
+<?php include 'assets/layout/head.php'; ?>
+<?php include 'assets/layout/pre_loader.php'; ?>
+<?php include 'assets/layout/navbar.php'; ?>
+<?php include 'assets/layout/sidebar.php';  ?>
+      <h1> hello</h1>
+<?php include 'assets/layout/script.php'; ?>
+
